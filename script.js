685d1d57a0732a294c9a2d6151c1158af6bcaa41
Benjamin Siegel <benjaminsiegel@172-30-34-254.DYNAPOOL.NYU.EDU>
2018-07-23 12:36:35 -0400

    without links

---
+++ script.js
@@ -0,0 +1,46 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Challenge 1: Add new input for user email in the submission page. Have this show up in the card. (BONUS: Make it clickable in the ad)
+
+
+
+// Challenge 2: Add a new input for the price of the good or service. Have this show up in the card.
+
+
+
+// Challenge 3: Save date and time of each posting when submitted. This should also be shown on the card (this should be done using javascript date functions)
+
+
+
+// Challenge 4: Have ads showing the most recent at the top and least recent at the bottom
+
+
+// Challenge 5: Make two different references in your database for services and one for goods (you will need to change the posting for to achieve this). Have these different references render on different web pages (you will need seperate html page for services and goods)
+
+
+
+// Challenge 6: Make ads searchable by price
+
+
+
+
+// SUPER BONUS HARD Challenge 6: Make a button that removes individual posting from database
